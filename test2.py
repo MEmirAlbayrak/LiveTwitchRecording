cfa6d9617c61f94d5773c98d98764b9129cedcb7
Emir Albayrak <emir.m.albayrak@gmail.com>
2021-09-24 00:45:48 +0300

    Change Name and delete py audio

--- test2.py
+++
@@ -1,4 +0,0 @@
-import os
-
-
-os.system('/Users/TaW/Documents/GitHub/LiveTwitchRecording/ffmpeg-20210728-0068b3d0f0-win64-static/bin/ffmpeg.exe'+ " -sseof -10 -i sylchasie_best.mp4 sylchasie_best_10.mp4")
