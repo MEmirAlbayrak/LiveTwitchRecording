05e4e28cb2ab3f5ed4bdbc5099b70356c3423f6b
Emir Albayrak <emir.m.albayrak@gmail.com>
2021-09-24 00:21:29 +0300

    GitIgnor Edit

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
-import streamlink
 import subprocess
-import time
 import sys
-import requests
+import time
 import os
-import json
 import datetime
-import getopt
 from multiprocessing import Process
-import shutil
 from pynput.keyboard import Key, Controller
 
 
@@ -17,8 +12,8 @@ class TwitchRecorder:
         self.projectFolder_Path = '/Users/TaW/Documents/GitHub/LiveTwitchRecording/'
         self.ffmpeg_path = '/Users/TaW/Documents/GitHub/LiveTwitchRecording/ffmpeg-20210728-0068b3d0f0-win64-static/bin/ffmpeg.exe'
 
-        self.username = "qcoat"
-        self.quality = "360p"
+        self.username = "sylchasie"
+        self.quality = "best"
 
         self.filename = self.username+"_"+self.quality+".mp4"
         self.temp_RecordFile = ""
@@ -27,35 +22,27 @@ class TwitchRecorder:
     
 
     def RecordStream(self):   
-        while True:
-
             self.temp_RecordFile = self.filename
-            subprocess.call(["streamlink", "twitch.tv/" + self.username, self.quality, "-o", self.temp_RecordFile , "-t" , "10 input"])
+            subprocess.call(["streamlink", "twitch.tv/" + self.username, self.quality,"-o", self.temp_RecordFile])
+            
+            
 
-    
 
     def SortFolder(self):
         isExist = os.path.exists(self.streamerFolderPath) 
-        if  isExist == False:
+        if  isExist == False:2
             os.mkdir(self.streamerFolderPath)
         else:
             print("There is already a file for this streamer.")
 
         while True:
-            time.sleep(10)
+            time.sleep(5)
             if(os.path.exists(self.projectFolder_Path+self.filename) is True):
                 try:
-                    subprocess.call([self.ffmpeg_path, '-err_detect', 'ignore_err', '-i', self.projectFolder_Path+self.filename, '-c', 'copy', os.path.join(self.streamerFolderPath, datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss")+self.filename)])
-                    os.remove(self.projectFolder_Path+self.filename)
-
-                    # fo = open(self.projectFolder_Path+self.filename, "wb")
-                    # fo.close()
-                    # keyboard = Controller()
-
-                    # print ("Name of the file: ", fo.name)
-                    # keyboard.press("q")
-                    # keyboard.release("q")
-                    # Close opend file
+                    date = datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss")
+                    os.system(self.ffmpeg_path+ " -sseof -4 -i " + self.projectFolder_Path+self.filename + " " + self.projectFolder_Path+"test"+self.filename)
+                    subprocess.call([self.ffmpeg_path, '-err_detect', 'ignore_err', '-i', self.projectFolder_Path+"test"+self.filename, '-c', 'copy', os.path.join(self.streamerFolderPath, date+self.filename)])
+                    os.remove(self.projectFolder_Path+"test"+self.filename)
                 except Exception as e:
                     print(e)
             else:
